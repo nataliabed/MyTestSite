aea7f8148f5683061681c3e3c9a19700ec579cd9
nataliabed <57414243+nataliabed@users.noreply.github.com>
2025-02-26 17:32:50 +0000

    TestUpload

---
+++ website-template-master/main.js
@@ -0,0 +1,34 @@
+let isDarkMode = false; // Track background state
+let isRotated = false; // Track rotation state
+
+function changeBackground() {
+    if (!isDarkMode) {
+        console.log('Switching to dark mode');
+        $('body').css({
+            'backgroundColor': 'black',
+            'color': 'white'
+        });
+    } else {
+        console.log('Switching to light mode');
+        $('body').css({
+            'backgroundColor': 'pink',
+            'color': 'black'
+        });
+    }
+    isDarkMode = !isDarkMode; // Toggle state
+}
+
+function rotate() {
+    if (!isRotated) {
+        console.log('Rotating');
+        $('body').css({
+            'transform': 'rotate(5deg)',
+        });
+    } else {
+        console.log('Rotating back');
+        $('body').css({
+            'transform': 'rotate(0deg)',
+        });
+    }
+    isRotated = !isRotated; // Toggle state
+}
